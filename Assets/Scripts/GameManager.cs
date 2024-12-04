f9e119b0b31f9d77f6ba3397e47d6a915a8e1705
배찬호 <1004chanho@gmail.com>
2024-12-04 18:19:02 +0900

    main
    main

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,177 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+// 점수와 게임 오버 여부, 게임 UI를 관리하는 게임 매니저
+public class GameManager : MonoBehaviourPunCallbacks, IPunObservable {
+    // 외부에서 싱글톤 오브젝트를 가져올때 사용할 프로퍼티
+    public static GameManager instance
+    {
+        get
+        {
+            // 만약 싱글톤 변수에 아직 오브젝트가 할당되지 않았다면
+            if (m_instance == null)
+            {
+                // 씬에서 GameManager 오브젝트를 찾아 할당
+                m_instance = FindObjectOfType<GameManager>();
+            }
+
+            // 싱글톤 오브젝트를 반환
+            return m_instance;
+        }
+    }
+
+    private static GameManager m_instance; // 싱글톤이 할당될 static 변수
+
+    public GameObject playerPrefab; // 생성할 플레이어 캐릭터 프리팹
+    public Text txtLogMsg;
+    public InputField ChatInput;
+    private int score = 0; // 현재 게임 점수
+    private GameObject whoami;
+    public bool isGameover { get; private set; } // 게임 오버 상태
+
+    // 주기적으로 자동 실행되는, 동기화 메서드
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info) {
+        // 로컬 오브젝트라면 쓰기 부분이 실행됨
+        if (stream.IsWriting)
+        {
+            // 네트워크를 통해 score 값을 보내기
+            stream.SendNext(score);
+        }
+        else
+        {
+            // 리모트 오브젝트라면 읽기 부분이 실행됨         
+
+            // 네트워크를 통해 score 값 받기
+            score = (int) stream.ReceiveNext();
+            // 동기화하여 받은 점수를 UI로 표시
+            UIManager.instance.UpdateScoreText(score);
+        }
+    }
+
+
+    private void Awake() {
+        // 씬에 싱글톤 오브젝트가 된 다른 GameManager 오브젝트가 있다면
+        
+        if (instance != this)
+        {
+            // 자신을 파괴
+            Destroy(gameObject);
+        }
+    }
+
+    // 게임 시작과 동시에 플레이어가 될 게임 오브젝트를 생성
+    IEnumerator Start() {
+        
+        // 생성할 랜덤 위치 지정
+        Vector3 randomSpawnPos = Random.insideUnitSphere * 5f;
+        // 위치 y값은 0으로 변경
+        randomSpawnPos.y = 0f;
+        
+        // 네트워크 상의 모든 클라이언트들에서 생성 실행
+        // 단, 해당 게임 오브젝트의 주도권은, 생성 메서드를 직접 실행한 클라이언트에게 있음
+        PhotonNetwork.Instantiate(playerPrefab.name, randomSpawnPos, Quaternion.identity);
+        string msg = "\n<color=#00ff00>[" + PhotonNetwork.NickName + "] Connected" + "</color>";
+        photonView.RPC("LogMsg", RpcTarget.AllBuffered, msg);
+        FindWhoAmI();
+        yield return new WaitForSeconds(1.0f);
+    }
+    public void FindWhoAmI()
+    {
+        GameObject[] ppl = FindObjectsOfType<GameObject>();
+        for(int i=0;i<ppl.Length;i++) { 
+            if (ppl[i].tag=="Player"&&ppl[i].GetComponent<PhotonView>().IsMine) { 
+                whoami = ppl[i]; 
+                break; 
+            } 
+        }
+        if (whoami != null) { Debug.Log("Local player object found: " + whoami.name); } 
+        else { Debug.LogWarning("Local player object not found."); }
+    }
+    // 점수를 추가하고 UI 갱신
+    public void AddScore(int newScore) {
+        // 게임 오버가 아닌 상태에서만 점수 증가 가능
+        if (!isGameover)
+        {
+            // 점수 추가
+            score += newScore;
+            // 점수 UI 텍스트 갱신
+            UIManager.instance.UpdateScoreText(score);
+        }
+    }
+
+    // 게임 오버 처리
+    public void EndGame() {
+        // 게임 오버 상태를 참으로 변경
+        isGameover = true;
+        // 게임 오버 UI를 활성화
+        UIManager.instance.SetActiveGameoverUI(true);
+    }
+
+    // 키보드 입력을 감지하고 룸을 나가게 함
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            PhotonNetwork.LeaveRoom();
+        }
+
+        if (ChatInput.isFocused)
+        {
+            if (!photonView.IsMine) return;
+            else if (photonView.IsMine)
+            {
+                whoami.GetComponent<PlayerMovement>().enabled = false;
+            }
+        }
+        else
+            whoami.GetComponent<PlayerMovement>().enabled = true;
+        
+        if (Input.GetKeyDown(KeyCode.Return) && !ChatInput.isFocused)
+        {
+            Send();
+        }
+    }
+    public void Send()
+    {
+        //채팅 전송할 때 인풋필드 내용이 있는지 검사, 없다면 무시
+        if (!string.IsNullOrEmpty(ChatInput.text))
+        {
+            string chatText = "\n<color=#00ff00>[" +
+                PhotonNetwork.NickName + "] : " + ChatInput.text +
+                "</color>";
+            photonView.RPC("ChatRPC", RpcTarget.AllBuffered, chatText);
+            ChatInput.text = "";
+        }
+        //채팅내용을 전송한 뒤 인풋필드 레이블을 비워주고,
+        //포커스를 다시 인풋필드에 맞춤
+        ChatInput.ActivateInputField();
+    }
+    [PunRPC]
+    void ChatRPC(string msg)
+    {
+
+        txtLogMsg.text += msg;
+
+    }
+    public void OnClickExitRoom()
+    {
+
+        string msg = "\n<color=#ff0000>[" + PhotonNetwork.NickName + "] Disconnected" + "</color>";
+        photonView.RPC("LogMsg", RpcTarget.AllBuffered, msg);
+
+        PhotonNetwork.LeaveRoom();
+    }
+    // 룸을 나갈때 자동 실행되는 메서드
+    public override void OnLeftRoom() {
+        // 룸을 나가면 로비 씬으로 돌아감
+        SceneManager.LoadScene("Lobby");
+    }
+    [PunRPC]
+    void LogMsg(string msg) //로그 메시지 Text UI에 텍스트를 누적시켜 표시
+    {
+        txtLogMsg.text = txtLogMsg.text + msg;
+    }
+}
---
+++ Assets/Scripts/LobbyManager.cs
@@ -0,0 +1,189 @@
+﻿using Photon.Pun; // 유니티용 포톤 컴포넌트들
+using Photon.Realtime; // 포톤 서비스 관련 라이브러리
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+// 마스터(매치 메이킹) 서버와 룸 접속을 담당
+public class LobbyManager : MonoBehaviourPunCallbacks {
+    private string gameVersion = "1"; // 게임 버전
+    
+    public Text connectionInfoText; // 네트워크 정보를 표시할 텍스트
+    public Button joinButton; // 룸 접속 버튼
+    public Button makeRoomButton; // 룸 접속 버튼
+    public InputField userId;
+    public InputField roomName;
+
+    public GameObject roomItem; // 룸 목록만큼 생성될 프리팹
+    public GameObject scrollContents;  //RoomItem이 차일드로 생성될 Parent 객체
+
+    private Dictionary<string, GameObject> rooms = new Dictionary<string, GameObject>();
+    // 게임 실행과 동시에 마스터 서버 접속 시도
+    private void Start() {
+        // 접속에 필요한 정보(게임 버전) 설정
+        PhotonNetwork.GameVersion = gameVersion;
+        // 설정한 정보를 가지고 마스터 서버 접속 시도
+        PhotonNetwork.ConnectUsingSettings();
+
+        // 룸 접속 버튼을 잠시 비활성화
+        joinButton.interactable = false;
+        makeRoomButton.interactable = false;
+
+        // 접속을 시도 중임을 텍스트로 표시
+        connectionInfoText.text = "마스터 서버에 접속중...";
+    }
+
+    // 마스터 서버 접속 성공시 자동 실행
+    public override void OnConnectedToMaster() {
+        // 룸 접속 버튼을 활성화
+        joinButton.interactable = true;
+        makeRoomButton.interactable = true;
+        // 접속 정보 표시
+        connectionInfoText.text = "온라인 : 마스터 서버와 연결됨";
+        
+    }
+
+    // 마스터 서버 접속 실패시 자동 실행
+    public override void OnDisconnected(DisconnectCause cause) {
+        // 룸 접속 버튼을 비활성화
+        joinButton.interactable = false;
+        // 접속 정보 표시
+        connectionInfoText.text = "오프라인 : 마스터 서버와 연결되지 않음\n접속 재시도 중...";
+
+        // 마스터 서버로의 재접속 시도
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    // 룸 접속 시도
+    public void Connect() {
+        // 중복 접속 시도를 막기 위해, 접속 버튼 잠시 비활성화
+        joinButton.interactable = false;
+        makeRoomButton.interactable = false;
+
+        // 마스터 서버에 접속중이라면
+        if (PhotonNetwork.IsConnected)
+        {
+            // 룸 접속 실행
+            connectionInfoText.text = "룸에 접속...";
+
+            string userName = userId.text;
+            PhotonNetwork.NickName = userName;//유저 이름 설정
+            PhotonNetwork.JoinRandomRoom();
+            
+        }
+        else
+        {
+            // 마스터 서버에 접속중이 아니라면, 마스터 서버에 접속 시도
+            connectionInfoText.text = "오프라인 : 마스터 서버와 연결되지 않음\n접속 재시도 중...";
+            // 마스터 서버로의 재접속 시도
+            PhotonNetwork.ConnectUsingSettings();
+        }
+    }
+
+    // (빈 방이 없어)랜덤 룸 참가에 실패한 경우 자동 실행
+    public override void OnJoinRandomFailed(short returnCode, string message) {
+        // 접속 상태 표시
+        connectionInfoText.text = "빈 방이 없음, 새로운 방 생성...";
+        // 최대 4명을 수용 가능한 빈방을 생성
+        PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = 4 });
+    }
+
+    // 룸에 참가 완료된 경우 자동 실행
+    public override void OnJoinedRoom() {
+        // 접속 상태 표시
+        connectionInfoText.text = "방 참가 성공";
+        // 모든 룸 참가자들이 Main 씬을 로드하게 함
+        PhotonNetwork.LoadLevel("Main");
+
+    }
+    public void OnClickJoinRandomRoom() //Join Random Room 버튼 연결 함수
+    {
+        PhotonNetwork.NickName = userId.text; //로컬플레이어 이름 설정
+        PlayerPrefs.SetString("USER_ID", userId.text); //플레이어 이름을 저장
+        PhotonNetwork.JoinRandomRoom();//무작위 방 입장
+    }
+    public void OnClickCreateRoom() //Make Room 버튼 연결 함수
+    {
+        string _roomName = roomName.text; //사용자가 입력 방제목을 얻어옴
+        if (string.IsNullOrEmpty(roomName.text)) // 사용자가 방제목을 입력 안했다면
+        {
+            _roomName = "Room_" + Random.Range(0, 999).ToString("000"); //랜덤하게 방제목 설정
+        }
+        PhotonNetwork.NickName = userId.text; //로컬 플레이어 이름 설정
+        PlayerPrefs.SetString("USER_ID", userId.text);
+
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.IsOpen = true;
+        roomOptions.IsVisible = true;
+        roomOptions.MaxPlayers = 20;
+
+        PhotonNetwork.CreateRoom(_roomName, roomOptions, TypedLobby.Default);
+    }
+    public override void OnCreateRoomFailed(short returnCode, string message) //방만들기 실패 했을 때 호출되는 콜백함수
+    {
+        Debug.Log("방 만들기 실패: " + message);
+    }
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        // 삭제된 RoomItem 프리팹을 저장할 임시변수
+        GameObject tempRoom = null;
+        
+        foreach (var roomInfo in roomList)
+        {
+            if (roomInfo.RemovedFromList == true)
+            {  // 룸이 삭제된 경우
+                // 딕셔너리에서 룸 이름으로 검색해 저장된 RoomItem 프리팹를 추출
+                rooms.TryGetValue(roomInfo.Name, out tempRoom);
+                Destroy(tempRoom); // RoomItem 프리팹 삭제        
+                rooms.Remove(roomInfo.Name); // 딕셔너리에서 해당 룸 이름의 데이터를 삭제
+                                             //GridLayoutGroup의 constraintCount 값을 RoomItem 갯수만큼 증가
+                scrollContents.GetComponent<GridLayoutGroup>().constraintCount = rooms.Count;
+                scrollContents.GetComponent<RectTransform>().sizeDelta -= new Vector2(0, 20);
+            }
+            else // 룸 정보가 변경된 경우
+            {
+                // 룸 이름이 딕셔너리에 없는 경우 새로 추가
+                if (rooms.ContainsKey(roomInfo.Name) == false)
+                {
+                    GameObject room = (GameObject)Instantiate(roomItem); //RoomItem 프리팹 동적 생성
+                    room.transform.SetParent(scrollContents.transform, false); // RoomItem을 scrollContents의 자식으로 설정
+
+                    RoomData roomData = room.GetComponent<RoomData>();
+                    roomData.roomName = roomInfo.Name; //방제목
+                    roomData.connectPlayer = roomInfo.PlayerCount; //현재인원수
+                    roomData.maxPlayer = roomInfo.MaxPlayers; //최대인원수
+                    roomData.DispRoomData();//텍스트 정보 표시
+
+                    roomData.GetComponent<UnityEngine.UI.Button>().onClick.AddListener(delegate
+                    {
+                        OnClickRoomItem(roomData.roomName);
+                    });
+
+                    // 딕셔너리 자료형에 데이터 추가
+                    rooms.Add(roomInfo.Name, room);
+                    //GridLayoutGroup의 constraintCount 값을 RoomItem 갯수만큼 증가
+                    scrollContents.GetComponent<GridLayoutGroup>().constraintCount = rooms.Count;
+                    //스크롤 영역의 높이를 증가
+                    scrollContents.GetComponent<RectTransform>().sizeDelta += new Vector2(0, 20);
+                }
+                else // 룸 이름이 딕셔너리에 없는 경우에 룸 정보를 갱신
+                {
+                    rooms.TryGetValue(roomInfo.Name, out tempRoom); //룸검색해 RoomItem을 tempRoom에 저장
+
+                    RoomData roomData = tempRoom.GetComponent<RoomData>();
+                    roomData.roomName = roomInfo.Name; //방제목
+                    roomData.connectPlayer = roomInfo.PlayerCount; //현재인원수
+                    roomData.maxPlayer = roomInfo.MaxPlayers; //최대인원수
+                    roomData.DispRoomData();//텍스트 정보 표시
+                }
+            }
+        }
+    }
+    void OnClickRoomItem(string roomName)
+    {
+        PhotonNetwork.NickName = userId.text; //방 접속한 플레이어 이름 설정
+        PlayerPrefs.SetString("USER_ID", userId.text); // 플레이어 이름 저장
+        PhotonNetwork.JoinRoom(roomName); // 방 제목으로 방 입장
+    }
+}
